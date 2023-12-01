3d5c99f18912bcdea1d93eb27a76ae9ac635d981
eenrics <ebenezeresh525@gmail.com>
2023-12-01 11:50:50 +0300

    nestjs mapped types

--- iluvcoffee/src/coffees/dto/update-coffee.dto.ts
+++ iluvcoffee/src/coffees/dto/update-coffee.dto.ts
@@ -1,6 +1,10 @@
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateCoffeeDto } from "./create-coffee.dto";
 // DTOs ARE USEFUL FOR DATA COMING IN AND GOING OUT
-export class UpdateCoffeeDto {
-    readonly name?: string;
-    readonly brand?: string;
-    readonly flavors?: string[];
-}
+// export class UpdateCoffeeDto {
+//     readonly name?: string;
+//     readonly brand?: string;
+//     readonly flavors?: string[];
+// }
+
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) { }
