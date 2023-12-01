859911849f2e42f726a25704996cfb2f973f0002
eenrics <ebenezeresh525@gmail.com>
2023-12-01 14:36:24 +0300

    join table with many to many relation

--- iluvcoffee/src/coffees/coffees.module.ts
+++ iluvcoffee/src/coffees/coffees.module.ts
@@ -3,11 +3,12 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 @Module({
     imports: [
         // MAKE TYPEORM MODULE AWARE OF THE ENTITIES
-        TypeOrmModule.forFeature([Coffee])
+        TypeOrmModule.forFeature([Coffee, Flavor])
     ],
     controllers: [CoffeesController],
     providers: [CoffeesController, CoffeesService],
