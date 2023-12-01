58dafb60a0e5eea543133d072c07a6c21d536c7f
eenrics <ebenezeresh525@gmail.com>
2023-12-01 11:40:28 +0300

    validators in nestjs

--- iluvcoffee/src/coffees/dto/create-coffee.dto.ts
+++ iluvcoffee/src/coffees/dto/create-coffee.dto.ts
@@ -1,6 +1,13 @@
 // IT IS BEST PRACTICE TO MAKE ALL DTO PROPERTIES AS READONLY TO HELP MAITAIN IMMUTABILITY
+import { IsString } from 'class-validator';
+
 export class CreateCoffeeDto {
+    @IsString()
     readonly name: string;
+
+    @IsString()
     readonly brand: string;
+
+    @IsString({ each: true })
     readonly flavors: string[];
 }
