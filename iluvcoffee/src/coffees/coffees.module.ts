37127e6747ce79a21aa27d36e35928cde8a28202
eenrics <ebenezeresh525@gmail.com>
2023-12-05 03:58:02 +0300

    provide and useValue

--- iluvcoffee/src/coffees/coffees.module.ts
+++ iluvcoffee/src/coffees/coffees.module.ts
@@ -6,13 +6,19 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
 
+
+class MockCoffeeService {
+    findAll() {
+        return 'mock-coffees';
+    }
+}
 @Module({
     imports: [
         // MAKE TYPEORM MODULE AWARE OF THE ENTITIES
         TypeOrmModule.forFeature([Coffee, Flavor, Event])
     ],
     controllers: [CoffeesController],
-    providers: [CoffeesController, CoffeesService],
+    providers: [CoffeesController, { provide: CoffeesService, useValue: new MockCoffeeService }],
     exports: [CoffeesService]
 })
 export class CoffeesModule { }
