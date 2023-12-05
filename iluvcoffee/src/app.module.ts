6fe8914bfa79f9ea8adb58ed940ecf191dde38d4
eenrics <ebenezeresh525@gmail.com>
2023-12-05 21:37:44 +0300

    config module

--- iluvcoffee/src/app.module.ts
+++ iluvcoffee/src/app.module.ts
@@ -3,21 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'pass123',
-    database: 'postgres',
-    // AN ENTITY REPRESENTS A RELATIONSHIP BETWEEN A DATABASE TABLE AND A TYPESCRIPT CLASS
-    // IN NESTJS AN ENTITY IS A CLASS DECORATED WITH @ENTITY() DECORATOR
-    autoLoadEntities: true,
-    // MAKE SURE TO DISABLE SYNCHRONIZE IN PRODUCTION
-    // AUTOMATICALLY SYNCHRONIZES ENTITIES AND METADATA WITH DATABASE SCHEMA / TABLES
-    synchronize: true,
-  })],
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      // AN ENTITY REPRESENTS A RELATIONSHIP BETWEEN A DATABASE TABLE AND A TYPESCRIPT CLASS
+      // IN NESTJS AN ENTITY IS A CLASS DECORATED WITH @ENTITY() DECORATOR
+      autoLoadEntities: true,
+      // MAKE SURE TO DISABLE SYNCHRONIZE IN PRODUCTION
+      // AUTOMATICALLY SYNCHRONIZES ENTITIES AND METADATA WITH DATABASE SCHEMA / TABLES
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
